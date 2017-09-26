156d3bcda39c0cf8f2b90273b3880a30836631d1
KamesCG <kamescg@gmail.com>
2017-09-26 15:03:10 -0400

    Performing Quick Save

--- src/interface/index.js
+++ src/interface/index.js
@@ -6,12 +6,11 @@ import React from 'react';
 /* ------------------------- Internal Dependencies -------------------------- */
 import Providers from 'providers'
 import Routes from 'store/routes/EntryRoutes';
-import {MiningInterface} from 'containers'
 /* ---------------------------- Initialization ------------------------------ */
 export default function Root({ store, history }) {
   return (
     <Providers store={store} history={history} >
-      <MiningInterface/>
+      <Routes />
     </Providers>
   );
 }
